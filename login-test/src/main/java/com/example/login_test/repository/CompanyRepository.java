5c999d8196995632d9ffac0e45560f5f8b68e70e
sgechocosk <shun.pc@outlook.jp>
2025-08-12 01:52:24 +0900

    Delete login-test directory

--- login-test/src/main/java/com/example/login_test/repository/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.login_test.repository;
-
-import com.example.login_test.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {}
